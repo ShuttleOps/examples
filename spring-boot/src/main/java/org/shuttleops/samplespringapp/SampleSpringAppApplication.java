707714729c4f708cfce0c5a25a4a703e86570b16
Yonathan Koren <yonkornilov@live.com>
2020-05-27 13:51:43 -0400

    Rename org in package path to shuttleops; fix Controller being able to serve html by changing from RestController to Controller, and adding thymeleaf (which was not available in 2.2.6).

--- spring-boot/src/main/java/org/shuttleops/samplespringapp/SampleSpringAppApplication.java
+++ spring-boot/src/main/java/org/shuttleops/samplespringapp/SampleSpringAppApplication.java
@@ -1,4 +1,4 @@
-package org.indellient.samplespringapp;
+package org.shuttleops.samplespringapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot/src/main/java/org/shuttleops/samplespringapp/controllers/WelcomeController.java
+++ spring-boot/src/main/java/org/shuttleops/samplespringapp/controllers/WelcomeController.java
@@ -1,9 +1,9 @@
-package org.indellient.samplespringapp.controllers;
+package org.shuttleops.samplespringapp.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController{
 	
 	@GetMapping("/")
