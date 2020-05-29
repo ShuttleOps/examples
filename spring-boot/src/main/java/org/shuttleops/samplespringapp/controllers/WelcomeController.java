94ff68936b49305fa66c2d97a7cc333418622156
Yonathan Koren <yonkornilov@live.com>
2020-05-29 00:52:25 -0400

    Add endpoint that shows IPv4, show IPv4 in make-unhealthy endpoint, so that we know which backend instance was made unhealthy.

--- spring-boot/src/main/java/org/shuttleops/samplespringapp/controllers/WelcomeController.java
+++ spring-boot/src/main/java/org/shuttleops/samplespringapp/controllers/WelcomeController.java
@@ -2,6 +2,7 @@ package org.shuttleops.samplespringapp.controllers;
 
 import org.shuttleops.samplespringapp.health.HealthToggle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,16 +17,25 @@ public class WelcomeController{
 	@Autowired
 	HealthToggle healthToggle;
 
+	@Value("${application.ipv4}")
+	private String ipv4;
+
 	@GetMapping( "/" )
 	public String index(){
 		return "index";
+	}
+
+	@GetMapping( "/ipv4" )
+	public String ipv4( Model model ){
+		model.addAttribute( "message", String.format( "The IPv4 address of this instance is: %s", ipv4) );
+		return "message";
 	}
 
 	@GetMapping( "/make-unhealthy" )
 	public String makeUnhealthy( Model model ){
 		int unhealthySeconds = Integer.parseInt( env.getProperty( "application.unhealthy_seconds" ) );
 		healthToggle.setUnhealthy( unhealthySeconds );
-		model.addAttribute( "message", String.format( "Health is now set to 'unhealthy' for %d seconds.", unhealthySeconds ) );
+		model.addAttribute( "message", String.format( "Health is now set to 'unhealthy' for %d seconds (IPv4: %s).", unhealthySeconds, ipv4 ) );
 		return "message";
 	}
 }
